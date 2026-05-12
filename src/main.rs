1388cab71b3a6827d2c19934917a89c604ba37d0
Remigit99 <aderemibiodun1000@gmail.com>
2026-05-12 12:24:52 +0100

    Implement Guess the Number Game

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 fn main() {
-    println!("Hello, world!");
-    println!("This is a simple Rust program, by Aderemi Abiodun!");
+    // println!("Hello, world!");
+    // println!("This is a simple Rust program, by Aderemi Abiodun!");
+
+    //**======= GUESS THE NUMBER GAME ======= */
+    use std::io;
+
+    println!("Welcome to the Guess the Number Game!");
+    println!("I'm thinking of a number between 1 and 100. Can you guess it?");
+
+    let mut guessed_number = String::new();
+    io::stdin().read_line(&mut guessed_number).expect("Something Went wrong!");
+    println!("You guessed: {}", guessed_number.trim());
 }
